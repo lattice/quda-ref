b9f75f8412e3e58167deaa7b66fae2f3f116b50e
rbabich <rbabich@bu.edu>
2011-02-28 16:29:59 -0800

    added "makedox" script to automate doxygen page generation

---
+++ mainpage.h
@@ -0,0 +1,8 @@
+
+/*! \mainpage QUDA Reference
+ *
+ * Here you can browse the <a href="files.html">files</a> and <a href="annotated.html">classes</a>
+ * internal to the QUDA library.
+ *
+ * <br><br><a href="http://lattice.github.com/quda">Return to the main QUDA page.</a>
+ */
